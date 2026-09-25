0c4d173327bc461b9821f8fc863d0e629bbaf671
Toño <huesjoan@gmail.com>
2026-09-24 23:17:31 -0600

    Convertir gastos en columnas

--- exportar_excel.php
+++ exportar_excel.php
@@ -70,7 +70,12 @@ if (!$result) {
                 <th>KM INICIAL</th>
                 <th>KM FINAL</th>
                 <th>KM TOTAL</th>
-                <th>DETALLE GASTOS</th>
+                <th style="background-color:#6A1B9A;">COMIDA</th>
+                <th style="background-color:#6A1B9A;">HOTEL</th>
+                <th style="background-color:#6A1B9A;">CASETAS</th>
+                <th style="background-color:#6A1B9A;">ESTACIONAMIENTO</th>
+                <th style="background-color:#6A1B9A;">GASOLINA</th>
+                <th style="background-color:#6A1B9A;">OTROS</th>
                 <th>TOTAL GASTOS</th>
                 <th>ESTATUS</th>
             </tr>
@@ -81,26 +86,55 @@ $cont = 0;
 while ($row = $result->fetch_assoc()) {
     $cont++;
     $clase = ($cont % 2 == 0) ? 'fila-alternativa' : '';
-    
+
     $f_inicio_formato = !empty($row['fecha_inicio']) ? date('d/m/Y', strtotime($row['fecha_inicio'])) : 'N/A';
     $h_ini_cruda = !empty($row['fecha_inicio']) ? date('H:i', strtotime($row['fecha_inicio'])) : '00:00';
     $h_inicio_formato = ($h_ini_cruda == '00:00') ? '<span class="alerta">Sin registrar</span>' : "<strong>$h_ini_cruda</strong>";
-    
+
     $id_ruta = $row['id'];
-    $sql_gastos = "SELECT concepto, SUM(monto) as total_concepto FROM gastos WHERE ruta_id = $id_ruta GROUP BY concepto";
+
+    // Obtener gastos agrupados por concepto
+    $sql_gastos = "SELECT concepto, SUM(monto) as total_concepto 
+                   FROM gastos 
+                   WHERE ruta_id = $id_ruta 
+                   GROUP BY concepto";
     $res_gastos = $conn->query($sql_gastos);
-    $detalle_gastos = [];
-    while ($g = $res_gastos->fetch_assoc()) {
-        $detalle_gastos[] = "<b>" . $g['concepto'] . ":</b> $" . number_format($g['total_concepto'], 2);
+
+    // Inicializar acumuladores
+    $comida = 0.0;
+    $hotel = 0.0;
+    $casetas = 0.0;
+    $estacionamiento = 0.0;
+    $gasolina = 0.0;
+    $otros = 0.0;
+
+    if ($res_gastos) {
+        while ($g = $res_gastos->fetch_assoc()) {
+            $concepto = mb_strtolower(trim($g['concepto']), 'UTF-8');
+            $monto = floatval($g['total_concepto']);
+
+            if (strpos($concepto, 'comida') !== false || strpos($concepto, 'alimento') !== false) {
+                $comida += $monto;
+            } elseif (strpos($concepto, 'hotel') !== false || strpos($concepto, 'hospedaje') !== false) {
+                $hotel += $monto;
+            } elseif (strpos($concepto, 'caseta') !== false || strpos($concepto, 'peaje') !== false) {
+                $casetas += $monto;
+            } elseif (strpos($concepto, 'estacionamiento') !== false || strpos($concepto, 'estacion') !== false || strpos($concepto, 'pension') !== false || strpos($concepto, 'pensión') !== false) {
+                $estacionamiento += $monto;
+            } elseif (strpos($concepto, 'gasolina') !== false || strpos($concepto, 'diesel') !== false || strpos($concepto, 'diésel') !== false || strpos($concepto, 'combustible') !== false) {
+                $gasolina += $monto;
+            } else {
+                $otros += $monto;
+            }
+        }
     }
-    $texto_gastos = empty($detalle_gastos) ? 'Sin gastos' : implode("<br>", $detalle_gastos);
 
     $ruta_num = htmlspecialchars($row['numero_ruta'] ?? 'Sin número');
-    $chofer = htmlspecialchars($row['chofer'] ?? '');
+    $chofer   = htmlspecialchars($row['chofer'] ?? '');
     $auxiliar = htmlspecialchars($row['auxiliar'] ?? 'Sin auxiliar');
     $vehiculo = htmlspecialchars($row['placas'] ?? '') . ' (' . htmlspecialchars($row['no_economico'] ?? '') . ')';
-    $origen = htmlspecialchars($row['origen'] ?? '');
-    
+    $origen   = htmlspecialchars($row['origen'] ?? '');
+
     $km_inicial = number_format($row['km_inicial'] ?? 0, 0, '.', '');
     $km_final   = number_format($row['km_final'] ?? 0, 0, '.', '');
     $km_total   = number_format($row['km_total'] ?? 0, 0, '.', '');
@@ -117,7 +151,12 @@ while ($row = $result->fetch_assoc()) {
                 <td style="text-align:center;"><?= $km_inicial ?></td>
                 <td style="text-align:center;"><?= $km_final ?></td>
                 <td style="text-align:center; font-weight:bold;"><?= $km_total ?> km</td>
-                <td><?= $texto_gastos ?></td>
+                <td style="text-align:right;">$<?= number_format($comida, 2) ?></td>
+                <td style="text-align:right;">$<?= number_format($hotel, 2) ?></td>
+                <td style="text-align:right;">$<?= number_format($casetas, 2) ?></td>
+                <td style="text-align:right;">$<?= number_format($estacionamiento, 2) ?></td>
+                <td style="text-align:right;">$<?= number_format($gasolina, 2) ?></td>
+                <td style="text-align:right;">$<?= number_format($otros, 2) ?></td>
                 <td style="text-align:right; font-weight:bold; color:#4A148C;">$<?= $total_gen ?></td>
                 <td style="text-align:center;"><?= ucfirst($row['estatus']) ?></td>
             </tr>
